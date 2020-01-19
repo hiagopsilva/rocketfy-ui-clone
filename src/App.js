ed3bf0eb9e0df70438e8be66668c7e6352a5362e
Hiago Prates <hiagohps360@gmail.com>
2020-01-19 15:30:50 -0300

    first commit
     Please enter the commit message for your changes. Lines starting
     On branch master
     Initial commit
    	new file:   .gitignore
    	new file:   README.md
    	new file:   debug.log
    	new file:   package.json
    	new file:   public/index.html
    	new file:   src/App.js
    	new file:   src/components/Board/context.js
    	new file:   src/components/Board/index.js
    	new file:   src/components/Board/styles.js
    	new file:   src/components/Card/index.js
    	new file:   src/components/Card/styles.js
    	new file:   src/components/Header/index.js
    	new file:   src/components/Header/styles.js
    	new file:   src/components/List/index.js
    	new file:   src/components/List/styles.js
    	new file:   src/index.js
    	new file:   src/services/api.js
    	new file:   src/styles/global.js
    	new file:   yarn.lock

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { DndProvider } from 'react-dnd';
+import HTML5backend from 'react-dnd-html5-backend';
+
+import GlobalStyle from './styles/global';
+
+import Header from './components/Header';
+import Board from './components/Board';
+
+function App() {
+  return (
+    <DndProvider backend={HTML5backend}>
+      <Header />
+      <Board />
+
+      <GlobalStyle />
+    </DndProvider>
+  );
+}
+
+export default App;
---
+++ src/components/Header/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 80px;
+  background: #7159C1;
+  color: #FFF;
+  
+  display: flex;
+  align-items: center;
+  
+`;
+
+// export const Texto = styled.h1`
+//   margin-left: 10px;
+// `;
